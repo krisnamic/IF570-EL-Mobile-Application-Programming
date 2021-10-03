c86424ef2ab08b863ee56501d7e8db8d0ad49ea5
Michael Krisna Cahyadi <cahyadi.michael.mkc24@gmail.com>
2021-10-03 14:30:50 +0700

    start week 7

---
+++ MichaelKrisnaCahyadi_00000033738_IF570_EL_UTS/app/src/main/java/id/ac/umn/michaelkrisnacahyadi_00000033738_if570_el_uts/ProfileActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.michaelkrisnacahyadi_00000033738_if570_el_uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
